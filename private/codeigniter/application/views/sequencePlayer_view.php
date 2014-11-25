b88cb08f00bfe66f125f77ccc3f26c48dabfccfb
Jem Mackay <jem@swarmtv.org>
2014-11-25 10:34:06 +0000

    tests the videoExtension.js file

--- private/codeigniter/application/views/sequencePlayer_view.php
+++ private/codeigniter/application/views/sequencePlayer_view.php
@@ -2,9 +2,9 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Sequence Player</title>
-<script type="application/javascript" src="../../../../../js/vendor/jquery-1.8.3.min0.js"></script>
+<script type="application/javascript" src="../../../../../js/vendor/jquery-1.8.3.min.js"></script>
 <script type="application/javascript" src="../../../../../js/popcorn.js"></script>
-<script type="application/javascript" src="../../../../../js/videoExtension.js"></script>
+<script type="application/javascript" src="../../../../../js/videoExtension0.js"></script>
 </head>
 
 <body>
