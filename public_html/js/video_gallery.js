a0c4b2846a658b5e29d8d4c812c98cdf7e5362f1
Jem Mackay <jem@swarmtv.org>
2014-11-27 18:01:26 +0000

    tidies up casing on letters

--- public_html/js/video_gallery.js
+++ public_html/js/video_gallery.js
@@ -22,13 +22,13 @@ $(document).ready(function() {
 		$(this).addClass('iframe');*/
 	});
 	
-	$('.videolink').click(function(){
+	$('.videoLink').click(function(){
 		var videoFile = $(this).attr('videofile');
 		var videoPoster = $(this).attr('videofile');
 		var videoWidth = Number($(this).attr('videowidth'));
 		var videoHeight = Number($(this).attr('videoheight'));
 		
-		var videocode = '<video width="'+videoWidth+'" height="'+videoHeight+'" controls autoplay autobuffer><source src="video/'+videoFile+'.ogv" type="video/ogg" /><source src="video/'+videoFile+'.mp4" type="video/mp4" /><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="'+videoWidth+'" height="'+(videoHeight+40)+'" id="video_player" align="middle"><param name="allowScriptAccess" value="sameDomain"><param name="allowFullScreen" value="true"><param name="movie" value="http:/video_player.swf?videoFile=video/'+videoFile+'.mp4&amp;skinFile=http:/video_skin.swf&amp;videoFileWidth='+videoWidth+'&amp;videoFileHeight='+videoHeight+'"><param name="quality" value="high"><param name="wmode" value="transparent"><param name="scale" value="noscale"><param name="salign" value="lt"><embed src="http:/video_player.swf?videoFile=video/'+videoFile+'.mp4&amp;skinFile=http:/video_skin.swf&amp;videoFileWidth='+videoWidth+'&amp;videoFileHeight='+videoHeight+'" quality="high" width="'+videoWidth+'" height="'+(videoHeight+40)+'" name="video_player" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" scale="noscale" salign="lt" wmode="transparent" allowfullscreen="true" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></video>'
+		var videoCode = '<video width="'+videoWidth+'" height="'+videoHeight+'" controls autoplay autobuffer><source src="video/'+videoFile+'.ogv" type="video/ogg" /><source src="video/'+videoFile+'.mp4" type="video/mp4" /><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="'+videoWidth+'" height="'+(videoHeight+40)+'" id="video_player" align="middle"><param name="allowScriptAccess" value="sameDomain"><param name="allowFullScreen" value="true"><param name="movie" value="http:/video_player.swf?videoFile=video/'+videoFile+'.mp4&amp;skinFile=http:/video_skin.swf&amp;videoFileWidth='+videoWidth+'&amp;videoFileHeight='+videoHeight+'"><param name="quality" value="high"><param name="wmode" value="transparent"><param name="scale" value="noscale"><param name="salign" value="lt"><embed src="http:/video_player.swf?videoFile=video/'+videoFile+'.mp4&amp;skinFile=http:/video_skin.swf&amp;videoFileWidth='+videoWidth+'&amp;videoFileHeight='+videoHeight+'" quality="high" width="'+videoWidth+'" height="'+(videoHeight+40)+'" name="video_player" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" scale="noscale" salign="lt" wmode="transparent" allowfullscreen="true" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></video>'
 		
 		$('#videoPlayer').html(videoCode);
 		
